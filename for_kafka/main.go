3b6cc69c3299eb7a2b6d955cc94ce64f0e0ae510
KiloByte <kirieshka2350@gmail.com>
2026-03-31 00:24:34 +0700

    доделал я до конца генератор, если что перед func main написанно как запускать

--- for_kafka/main.go
+++ for_kafka/main.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"log"
 	"math/big"
+	"net"
 	"strconv"
 	"time"
 
@@ -23,19 +24,62 @@ type OrderCreated struct {
 	Product string `json:"product"`
 }
 
+type orderCanceled struct {
+	OrderID string `json:"order_id"`
+	Who     string `json:"who_created"`
+	UserID  string `json:"user_id"`
+	Product string `json:"product"`
+}
+
+type OrderPurchaised struct {
+	Who    string `json:"who_created"`
+	UserID string `json:"user_id"`
+}
+
 // запуск
 // ./main -consumer=true -group abc, обязательно у -consumer должно стоять равно иначе произойдёт невкусное
+// запускаем короче просто main
+// потом пишем для каждого нового
+// ./main -consumer=true -group *вставьте имя группы* -topic=[0:2]
 func main() {
 	var is_cons bool
 	var group_id string
 	var ofst int64
-	var topic string
+	var tpc int
+	topics := []string{"order-created", "order-purchased", "order-canceled"}
 	flag.BoolVar(&is_cons, "consumer", false, "set consumer or producer mode")
 	flag.StringVar(&group_id, "group", "first", "set group id. Only use in consumer mode")
 	flag.Int64Var(&ofst, "ofset", 0, "set ofset of reading topic, use only in consumer mode")
-	flag.StringVar(&topic, "topic", "order-created", "set topic to write/read")
+	flag.IntVar(&tpc, "topic", 0, "set one of three topics, only numbers [0:2]")
 	flag.Parse()
+	if tpc > 2 || tpc < 0 {
+		tpc = 0
+	}
 	kafkas := []string{"localhost:9092", "localhost:9093", "localhost:9094"}
+	conn, err := kafka.Dial("tcp", "localhost:9092")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer conn.Close()
+
+	controller, err := conn.Controller()
+	if err != nil {
+		panic(err.Error())
+	}
+	controllerConn, err := kafka.Dial("tcp", net.JoinHostPort(controller.Host, strconv.Itoa(controller.Port)))
+	if err != nil {
+		panic(err.Error())
+	}
+	defer controllerConn.Close()
+
+	topicConfigs := []kafka.TopicConfig{{Topic: "order-created", NumPartitions: 5, ReplicationFactor: 3},
+		{Topic: "order-canceled", NumPartitions: 5, ReplicationFactor: 3}, {Topic: "reder-purchaised", NumPartitions: 5, ReplicationFactor: 3}}
+
+	err = controllerConn.CreateTopics(topicConfigs...)
+	if err != nil {
+		panic(err.Error())
+	}
+
 	if is_cons == false {
 		// producer
 
@@ -43,65 +87,154 @@ func main() {
 		// по дефолту эта настройка должна быть включена
 		what_created := []string{"printer", "scaner", "cable", "monitor", "pc", "ic", "acdc"}
 		order_id := 1
-		kafka_w := &kafka.Writer{Addr: kafka.TCP(kafkas...), Topic: topic, WriteTimeout: 10 * time.Second,
+		kafka_w_f := &kafka.Writer{Addr: kafka.TCP(kafkas...), Topic: topics[0], WriteTimeout: 10 * time.Second,
 			Balancer: &kafka.Hash{}, AllowAutoTopicCreation: true, ReadTimeout: 5 * time.Second, MaxAttempts: 10}
-		defer kafka_w.Close()
+		kafka_w_s := &kafka.Writer{Addr: kafka.TCP(kafkas...), Topic: topics[1], WriteTimeout: 10 * time.Second,
+			Balancer: &kafka.Hash{}, AllowAutoTopicCreation: true, ReadTimeout: 5 * time.Second, MaxAttempts: 10}
+		kafka_w_t := &kafka.Writer{Addr: kafka.TCP(kafkas...), Topic: topics[2], WriteTimeout: 10 * time.Second,
+			Balancer: &kafka.Hash{}, AllowAutoTopicCreation: true, ReadTimeout: 5 * time.Second, MaxAttempts: 10}
+
+		defer kafka_w_f.Close()
 		who_purchase := []string{"Ivan", "Makson", "Andrew", "Vladimir", "Vladislav", "Olga", "Eva"} // место в списке = orderID по которому будет ключ
 		l := big.NewInt(int64(len(who_purchase)))
+		m := big.NewInt(100)
 		time.Sleep(10000)
 		for true {
+			t, _ := rand.Int(rand.Reader, m)
 			timestamp := time.Now()
 			who, _ := rand.Int(rand.Reader, l)
 			what, _ := rand.Int(rand.Reader, l)
-			payload := OrderCreated{
-				OrderID: strconv.Itoa(order_id),
-				Amount:  1,
-				Who:     who_purchase[who.Int64()],
-				UserID:  who.String(),
-				Product: what_created[what.Int64()],
+			if t.Cmp(big.NewInt(30)) == -1 {
+				payload := OrderCreated{
+					OrderID: strconv.Itoa(order_id),
+					Amount:  1,
+					Who:     who_purchase[who.Int64()],
+					UserID:  who.String(),
+					Product: what_created[what.Int64()],
+				}
+				to_write, _ := json.Marshal(payload)
+				msg := kafka.Message{
+					Key:   []byte(who.String()),
+					Value: to_write,
+					Time:  timestamp,
+					Headers: []kafka.Header{
+						{
+							Key:   "eventType",
+							Value: []byte("OrderCreated"),
+						},
+						{
+							Key:   "eventID",
+							Value: []byte("0"),
+						},
+						{
+							Key:   "entityID",
+							Value: who.Bytes(),
+						},
+						{
+							Key:   "Source",
+							Value: []byte("from Go"),
+						},
+						{
+							Key:   "version",
+							Value: []byte("1"),
+						},
+						{
+							Key:   "timestamp",
+							Value: []byte(time.Now().Format(time.RFC3339)),
+						},
+					},
+				}
+				err := kafka_w_f.WriteMessages(context.Background(), msg)
+				if err != nil {
+					log.Fatal("failed to write messages: ", err)
+				}
+				order_id++
+				fmt.Print("Order Created ", payload, "\n")
+			} else if t.Cmp(big.NewInt(60)) == -1 {
+				payload := OrderPurchaised{
+					Who:    who_purchase[who.Int64()],
+					UserID: who.String(),
+				}
+				to_write, _ := json.Marshal(payload)
+				msg := kafka.Message{
+					Key:   []byte(who.String()),
+					Value: to_write,
+					Time:  timestamp,
+					Headers: []kafka.Header{
+						{
+							Key:   "eventType",
+							Value: []byte("OrderPurchaised"),
+						},
+						{
+							Key:   "eventID",
+							Value: []byte("0"),
+						},
+						{
+							Key:   "entityID",
+							Value: who.Bytes(),
+						},
+						{
+							Key:   "Source",
+							Value: []byte("from Go"),
+						},
+						{
+							Key:   "version",
+							Value: []byte("1"),
+						},
+						{
+							Key:   "timestamp",
+							Value: []byte(time.Now().Format(time.RFC3339)),
+						},
+					},
+				}
+				kafka_w_s.WriteMessages(context.TODO(), msg)
+				fmt.Print("Order Purchaised ", payload, "\n")
+			} else {
+				payload := orderCanceled{
+					Who:     who_purchase[who.Int64()],
+					UserID:  who.String(),
+					OrderID: strconv.Itoa(order_id),
+					Product: what_created[what.Int64()],
+				}
+				to_write, _ := json.Marshal(payload)
+				msg := kafka.Message{
+					Key:   []byte(who.String()),
+					Value: to_write,
+					Time:  timestamp,
+					Headers: []kafka.Header{
+						{
+							Key:   "eventType",
+							Value: []byte("OrderCanceled"),
+						},
+						{
+							Key:   "eventID",
+							Value: []byte("0"),
+						},
+						{
+							Key:   "entityID",
+							Value: who.Bytes(),
+						},
+						{
+							Key:   "Source",
+							Value: []byte("from Go"),
+						},
+						{
+							Key:   "version",
+							Value: []byte("1"),
+						},
+						{
+							Key:   "timestamp",
+							Value: []byte(time.Now().Format(time.RFC3339)),
+						},
+					},
+				}
+				kafka_w_t.WriteMessages(context.TODO(), msg)
+				fmt.Print("Order Canceled ", payload, "\n")
 			}
-			to_write, _ := json.Marshal(payload)
-			msg := kafka.Message{
-				Key:   []byte(who.String()),
-				Value: to_write,
-				Time:  timestamp,
-				Headers: []kafka.Header{
-					{
-						Key:   "eventType",
-						Value: []byte("OrderCreated"),
-					},
-					{
-						Key:   "eventID",
-						Value: []byte("0"),
-					},
-					{
-						Key:   "entityID",
-						Value: who.Bytes(),
-					},
-					{
-						Key:   "Source",
-						Value: []byte("from Go"),
-					},
-					{
-						Key:   "version",
-						Value: []byte("1"),
-					},
-					{
-						Key:   "timestamp",
-						Value: []byte(time.Now().Format(time.RFC3339)),
-					},
-				},
-			}
-			err := kafka_w.WriteMessages(context.Background(), msg)
-			if err != nil {
-				log.Fatal("failed to write messages: ", err)
-			}
-			order_id++
 			time.Sleep(time.Duration(5000))
-			fmt.Println(payload)
 		}
 	} else {
-		r_cfg := kafka.ReaderConfig{Brokers: kafkas, GroupID: group_id, Topic: topic, StartOffset: ofst}
+		r_cfg := kafka.ReaderConfig{Brokers: kafkas, GroupID: group_id, Topic: topics[tpc], StartOffset: ofst}
 		kafka_r := kafka.NewReader(r_cfg)
 		for true {
 			// Fetch+Commit -> больше контроля за тем что мы отсмотрели
