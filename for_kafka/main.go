c6a3ffe85b2fa67ea975f0a32071062da2102d62
KiloByte <kirieshka2350@gmail.com>
2026-03-30 23:29:33 +0700

    сделал потребителя в go, также написал запуск для него

--- for_kafka/main.go
+++ for_kafka/main.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"crypto/rand"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
 	"log"
 	"math/big"
 	"strconv"
@@ -21,71 +23,116 @@ type OrderCreated struct {
 	Product string `json:"product"`
 }
 
+// запуск
+// ./main -consumer=true -group abc, обязательно у -consumer должно стоять равно иначе произойдёт невкусное
 func main() {
-	fmt.Println("abc")
-	// producer
+	var is_cons bool
+	var group_id string
+	flag.BoolVar(&is_cons, "consumer", false, "set consumer or producer mode")
+	flag.StringVar(&group_id, "group", "first", "set group id. Only use in consumer mode")
+	flag.Parse()
 	topic := "order-created"
-	// при включённом автосоздании топиков он сам создатся и мы к нему подключимся и будем писать в него
-	// по дефолту эта настройка должна быть включена
 	kafkas := []string{"localhost:9092", "localhost:9093", "localhost:9094"}
-	what_created := []string{"printer", "scaner", "cable", "monitor", "pc", "ic", "acdc"}
-	order_id := 1
-	kafka_w := &kafka.Writer{Addr: kafka.TCP(kafkas...), Topic: topic, WriteTimeout: 10 * time.Second,
-		Balancer: &kafka.Hash{}, AllowAutoTopicCreation: true, ReadTimeout: 5 * time.Second, MaxAttempts: 10}
-	defer kafka_w.Close()
-	who_purchase := []string{"Ivan", "Makson", "Andrew", "Vladimir", "Vladislav", "Olga", "Eva"} // место в списке = orderID по которому будет ключ
-	l := big.NewInt(int64(len(who_purchase)))
-	time.Sleep(10000)
-	for true {
-		timestamp := time.Now()
-		who, _ := rand.Int(rand.Reader, l)
-		what, _ := rand.Int(rand.Reader, l)
-		payload := OrderCreated{
-			OrderID: strconv.Itoa(order_id),
-			Amount:  1,
-			Who:     who_purchase[who.Int64()],
-			UserID:  who.String(),
-			Product: what_created[what.Int64()],
+	if is_cons == false {
+		// producer
+
+		// при включённом автосоздании топиков он сам создатся и мы к нему подключимся и будем писать в него
+		// по дефолту эта настройка должна быть включена
+		what_created := []string{"printer", "scaner", "cable", "monitor", "pc", "ic", "acdc"}
+		order_id := 1
+		kafka_w := &kafka.Writer{Addr: kafka.TCP(kafkas...), Topic: topic, WriteTimeout: 10 * time.Second,
+			Balancer: &kafka.Hash{}, AllowAutoTopicCreation: true, ReadTimeout: 5 * time.Second, MaxAttempts: 10}
+		defer kafka_w.Close()
+		who_purchase := []string{"Ivan", "Makson", "Andrew", "Vladimir", "Vladislav", "Olga", "Eva"} // место в списке = orderID по которому будет ключ
+		l := big.NewInt(int64(len(who_purchase)))
+		time.Sleep(10000)
+		for true {
+			timestamp := time.Now()
+			who, _ := rand.Int(rand.Reader, l)
+			what, _ := rand.Int(rand.Reader, l)
+			payload := OrderCreated{
+				OrderID: strconv.Itoa(order_id),
+				Amount:  1,
+				Who:     who_purchase[who.Int64()],
+				UserID:  who.String(),
+				Product: what_created[what.Int64()],
+			}
+			to_write, _ := json.Marshal(payload)
+			msg := kafka.Message{
+				Key:   []byte(who.String()),
+				Value: to_write,
+				Time:  timestamp,
+				Headers: []kafka.Header{
+					{
+						Key:   "eventType",
+						Value: []byte("OrderCreated"),
+					},
+					{
+						Key:   "eventID",
+						Value: []byte("0"),
+					},
+					{
+						Key:   "entityID",
+						Value: who.Bytes(),
+					},
+					{
+						Key:   "Source",
+						Value: []byte("from Go"),
+					},
+					{
+						Key:   "version",
+						Value: []byte("1"),
+					},
+					{
+						Key:   "timestamp",
+						Value: []byte(time.Now().Format(time.RFC3339)),
+					},
+				},
+			}
+			err := kafka_w.WriteMessages(context.Background(), msg)
+			if err != nil {
+				log.Fatal("failed to write messages: ", err)
+			}
+			order_id++
+			time.Sleep(time.Duration(5000))
+			fmt.Println(payload)
 		}
-		to_write, _ := json.Marshal(payload)
-		msg := kafka.Message{
-			Key:   []byte(who.String()),
-			Value: to_write,
-			Time:  timestamp,
-			Headers: []kafka.Header{
-				{
-					Key:   "eventType",
-					Value: []byte("OrderCreated"),
-				},
-				{
-					Key:   "eventID",
-					Value: []byte("0"),
-				},
-				{
-					Key:   "entityID",
-					Value: who.Bytes(),
-				},
-				{
-					Key:   "Source",
-					Value: []byte("from Go"),
-				},
-				{
-					Key:   "version",
-					Value: []byte("1"),
-				},
-				{
-					Key:   "timestamp",
-					Value: []byte(time.Now().Format(time.RFC3339)),
-				},
-			},
+	} else {
+		r_cfg := kafka.ReaderConfig{Brokers: kafkas, GroupID: group_id, Topic: topic}
+		kafka_r := kafka.NewReader(r_cfg)
+		for true {
+			// Fetch+Commit -> больше контроля за тем что мы отсмотрели
+			// msg, err := kafka_r.FetchMessage(context.TODO())
+			// if err != nil {
+			// 	kafka_r.CommitMessages(context.TODO(), msg)
+			// }
+			for kafka_r.Lag() > 15 {
+				msg, err := kafka_r.ReadLag(context.TODO())
+				if err != nil {
+					if err != io.EOF {
+						msg, err = kafka_r.ReadLag(context.TODO())
+					} else {
+						return
+					}
+					if err != nil {
+						fmt.Errorf("error occured: %s", err.Error())
+					}
+				}
+				fmt.Printf("kafka read lag: %d\n", msg)
+			}
+			msg, err := kafka_r.ReadMessage(context.TODO())
+			if err != nil {
+				if err != io.EOF {
+					msg, err = kafka_r.ReadMessage(context.TODO())
+				} else {
+					return
+				}
+				if err != nil {
+					fmt.Errorf("error occured: %s", err.Error())
+				}
+			}
+			fmt.Printf("kafka read message %s\n", msg.Value)
 		}
-		err := kafka_w.WriteMessages(context.Background(), msg)
-		if err != nil {
-			log.Fatal("failed to write messages: ", err)
-		}
-		order_id++
-		time.Sleep(time.Duration(5000))
-		fmt.Println(payload)
 	}
 	//consumer
 	// topic := "my-topica"
